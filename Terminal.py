e77fe65762339eb96b52f7fe48afb91928ac30d0
etvan13 <etvan13@gmail.com>
2024-12-04 19:54:12 -0600

    disclaimer updates

--- Terminal.py
+++ Terminal.py
@@ -1,3 +1,11 @@
+"""
+DISCLAIMER:
+This program is intended solely for educational purposes and the testing of your own software's security.
+Use of this tool on unauthorized systems or for illegal purposes is strictly prohibited.
+
+The developers are not responsible for any misuse or damage caused by this tool.
+"""
+
 # List needed imports
 import os
 import sys
@@ -25,8 +33,8 @@ class Terminal:
 
         self.commands = {
             "help": self.show_help,
+            "disclaimer": self.show_disclaimer,
             "greetings": self.greet,
-            "skeleton" : self.skeletonCommand,
             "simple bruteforce": self.simple_brute_force_command,
             "parameter bruteforce": self.parameter_brute_force_command,
             # Additional commands can be added here
@@ -67,7 +75,6 @@ class Terminal:
             print(self.process_command(command_input))
 
 
-
     ####COMMANDS####
     
     # Prints a list of available commands
@@ -76,6 +83,22 @@ class Terminal:
         help_text = "\n".join(f"- {cmd}" for cmd in command_list)
         return help_text
     
+    # Returns Disclaimer
+    def show_disclaimer(self):
+        disclaimer = """
+    **************************************
+    DISCLAIMER:
+    This program is intended solely for educational purposes 
+    and the testing of your own software's security.Unauthorized 
+    use, including testing systems without permission, is 
+    strictly prohibited.
+
+    The developers are not responsible for any misuse or damage 
+    caused by this tool. Check the 'README.md' for more.
+    **************************************
+        """
+        return disclaimer
+        
     # Returns a greeting
     def greet(self):
         return "Hello Universe!"
@@ -95,22 +118,7 @@ class Terminal:
 
 
 
-
-
-    # **************** HELPFUL REFERENCES *****************
-    # Runs the skeleton class
-    def skeletonCommand(self) :
-        skeletonObject = SkeleClass(self, self.counter) # The additional 'self.counter' just allows use with the coordinates
-        skeletonObject.run() # Runs the 'run()' command in the skeleton class
-        return "Back to main terminal." # Message for returning to the terminal (after the run function finishes, it means your command finished)
-    # ********************************************
-
-    # Concept for adding external commands
-    def add_external_command(self, command_name, command_function):
-        self.commands[command_name] = command_function
-
-    ##################
-
+### Example command class ###
 class SkeleClass:
     def __init__(self):
         pass  # Initialize any required variables here
